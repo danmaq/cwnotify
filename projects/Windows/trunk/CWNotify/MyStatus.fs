ed72ed73bec74151d78b6e08ead08fa6bbb281b5
danmaq <danmaq@a959c83c-b6d5-48ed-9b59-49efaf540b30>
2014-02-17 10:08:41 +0000

    とりあえず30秒に1度API出力を受信して新着があればバルーン表示するようになった。 インストーラ用のスクリプト作り始めたが、本当に作り始めたばかりなのでまだ動かない。

---
+++ projects/Windows/trunk/CWNotify/MyStatus.fs
@@ -0,0 +1,45 @@
+﻿module MyStatus
+
+open System
+open System.IO
+open System.Runtime.Serialization
+open System.Runtime.Serialization.Json
+open System.Text
+
+/// <summary>Response of "my/status"</summary>
+[<DataContract>]
+type MyStatus() =
+    [<DataMember>]
+    member val unread_room_num = 0 with get, set
+    [<DataMember>]
+    member val mention_room_num = 0 with get, set
+    [<DataMember>]
+    member val mytask_room_num = 0 with get, set
+
+    /// <summary>Unread messages</summary>
+    [<DataMember>]
+    member val unread_num = 0 with get, set
+    [<DataMember>]
+    member val mention_num = 0 with get, set
+
+    /// <summary>Numbers of total tasks.</summary>
+    [<DataMember>]
+    member val mytask_num = 0 with get, set
+
+    /// <summary>Returns a string that represents the current object.</summary>
+    /// <returns>A string that represents the current object.</returns>
+    override this.ToString() =
+        String.Format(
+            "MyStatus[unread_room_num:{0}, mention_room_num:{1}, mytask_room_num:{2}, unread_num:{3}, mention_num:{4}, mytask_num:{5}",
+            this.unread_room_num, this.mention_room_num, this.mytask_room_num, this.unread_num, this.mention_num, this.mytask_num)
+        
+
+/// <summary>Parsing JSON Strings.</summary>
+/// <param name="json">JSON Strings.</param>
+/// <returns>If parsed JSON correctly, will return instance of <c>MyStatus</c>.</returns>
+let ParseJSON (json:string) =
+    use stream = new MemoryStream(json |> Encoding.UTF8.GetBytes)
+    let serializer = new DataContractJsonSerializer(typeof<MyStatus>)
+    match stream |> serializer.ReadObject with
+        | :? MyStatus as result -> Some result
+        | _ -> None
