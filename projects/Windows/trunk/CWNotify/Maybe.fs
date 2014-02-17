e810465a1b799d2d8ac2fef994511eac38e399fa
danmaq <info@danmaq.com>
2014-02-17 01:39:15 +0000

    多重起動防止、バージョン情報埋め込み、タスクトレイ処理分離。

--- projects/Windows/trunk/CWNotify/Maybe.fs
+++ projects/Windows/trunk/CWNotify/Maybe.fs
@@ -1,18 +1,17 @@
-﻿namespace Danmaq.CWNotify
+﻿/// <summary>Maybe Monad.</summary>
+module Maybe
+
+type MaybeBuilder() =
+
+    /// <summary>Called for <c>let!</c> and <c>do!</c> in computation expressions.</summary>
+    member this.Bind(x, f) =
+        match x with
+            | Some y -> f y
+            | None -> None
+
+    /// <summary>Called for <c>return!</c> in computation expressions.</summary>
+    member this.Return(x) =
+        x |> Some
 
 /// <summary>Maybe Monad.</summary>
-module Maybe =
-    type MaybeBuilder() =
-
-        /// <summary>Called for <c>let!</c> and <c>do!</c> in computation expressions.</summary>
-        member this.Bind(x, f) =
-            match x with
-                | Some y -> f y
-                | None -> None
-
-        /// <summary>Called for <c>return!</c> in computation expressions.</summary>
-        member this.Return(x) =
-            x |> Some
-
-    /// <summary>Maybe Monad.</summary>
-    let maybe = MaybeBuilder()
+let maybe = MaybeBuilder()
---
+++ projects/Windows/trunk/CWNotify/Notify.fs
@@ -0,0 +1,72 @@
+﻿module Notify
+
+open System
+open System.Drawing
+open System.Reflection
+open System.Runtime.InteropServices
+open System.Windows.Forms
+
+/// <summary>
+/// Retrieves a handle to the foreground window (the window with which the user is currently working).
+/// </summary>
+/// <returns>The return value is a handle to the foreground window.</returns>
+[<DllImport(@"user32")>]
+extern IntPtr GetForegroundWindow();
+
+/// <summary>Returns the Windows desktop window.</summary>
+/// <returns>Identifies the Windows desktop window.</returns>
+[<DllImport(@"user32")>]
+extern IntPtr GetDesktopWindow();
+
+/// <summary>
+/// Brings the thread that created the specified window into the foreground and activates the window.
+/// </summary>
+/// <param name="hWnd">
+/// A handle to the window that should be activated and brought to the foreground. 
+/// </param>
+/// <returns>If the window was brought to the foreground, the return value is nonzero.</returns>
+[<DllImport(@"user32")>]
+extern [<MarshalAs(UnmanagedType.Bool)>] bool SetForegroundWindow(IntPtr hWnd);
+
+/// <summary>
+/// display a notification icon. If icon fails to display, and then retry automatically.
+/// </summary>
+/// <param name="notify">Instance of notification icon.</param>
+/// <param name="prevTick">previous tick time.</param>
+let rec viewIcon (notify: NotifyIcon) prevTick =
+    notify.Visible <- true
+    let tick = Environment.TickCount - prevTick
+    if tick >= 4000 then
+        notify.Visible <- false
+        viewIcon notify tick
+
+/// <summary>
+/// Notification icon will not disappear even if pressing the Alt + F4 key.
+/// </summary>
+/// <param name="notify">Instance of notification icon.</param>
+/// <param name="cms">Instance of context menu.</param>
+let cancelAltF4 (notify: NotifyIcon) (cms:ContextMenuStrip) =
+    let setForeground = fun () -> SetForegroundWindow(GetDesktopWindow()) |> ignore
+    cms.PreviewKeyDown.Add <|
+        fun args -> if args.Alt then setForeground()
+    cms.Closing.Add <|
+        fun args ->
+            let fi =
+                typeof<NotifyIcon>.GetField(
+                    "window", BindingFlags.NonPublic ||| BindingFlags.Instance)
+            if GetForegroundWindow() = (fi.GetValue(notify) :?> NativeWindow).Handle
+            then setForeground()
+
+let start appName =
+    let notify = new NotifyIcon()
+    //notify.BalloonTipText <- "FUGA"
+    notify.Text <- appName;
+    notify.ContextMenuStrip <-
+        let cms = new ContextMenuStrip()
+        cancelAltF4 notify cms
+        ("E&xit" |> cms.Items.Add).Click.Add <| fun args -> Application.Exit()
+        cms
+    notify.Icon <- SystemIcons.Application
+    viewIcon notify Environment.TickCount
+    Application.ApplicationExit.Add <| fun args -> notify.Dispose()
+    Application.Run()
