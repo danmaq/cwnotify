324e9ea6315e1b2db35fecf906798a02dbdc4359
danmaq <info@danmaq.com>
2014-02-18 05:50:04 +0000

    ■v0.1.0.0 起動前にユーザー環境変数CW_API_KEYを作成し、そこにChatWork APIキーを入れてください。 起動するとタスクトレイに常駐します。 30秒ごとに未読確認をします。

---
+++ projects/Windows/tags/20140218_0.1.0.0/CWNotify/MyStatus.fs
@@ -0,0 +1,44 @@
+﻿module MyStatus
+
+open Printf
+open System.IO
+open System.Runtime.Serialization
+open System.Runtime.Serialization.Json
+open System.Text
+
+/// <summary>Response of "my/status"</summary>
+[<DataContract>]
+type MyStatus() =
+    [<DataMember>]
+    member val unread_room_num = 0 with get, set
+    [<DataMember>]
+    member val mention_room_num = 0 with get, set
+    [<DataMember>]
+    member val mytask_room_num = 0 with get, set
+
+    /// <summary>Unread messages</summary>
+    [<DataMember>]
+    member val unread_num = 0 with get, set
+    [<DataMember>]
+    member val mention_num = 0 with get, set
+
+    /// <summary>Numbers of total tasks.</summary>
+    [<DataMember>]
+    member val mytask_num = 0 with get, set
+
+    /// <summary>Returns a string that represents the current object.</summary>
+    /// <returns>A string that represents the current object.</returns>
+    override this.ToString() =
+        sprintf
+            "MyStatus[unread_room_num:%d, mention_room_num:%d, mytask_room_num:%d, unread_num:%d, mention_num:%d, mytask_num:%d"
+            this.unread_room_num this.mention_room_num this.mytask_room_num this.unread_num this.mention_num this.mytask_num
+
+/// <summary>Parsing JSON Strings.</summary>
+/// <param name="json">JSON Strings.</param>
+/// <returns>If parsed JSON correctly, will return instance of <c>MyStatus</c>.</returns>
+let ParseJSON (json:string) =
+    use stream = new MemoryStream(json |> Encoding.UTF8.GetBytes)
+    let serializer = new DataContractJsonSerializer(typeof<MyStatus>)
+    match stream |> serializer.ReadObject with
+        | :? MyStatus as result -> Some result
+        | _ -> None
---
+++ projects/Windows/tags/20140218_0.1.0.0/CWNotify/Program.fs
@@ -0,0 +1,32 @@
+﻿module Program
+
+open System
+open System.Threading
+open System.Timers
+open System.Windows.Forms
+
+[<Literal>]
+let APP_NAME = "ChatWork Notify";
+
+[<Literal>]
+let CW_API_KEY = "CW_API_KEY";
+
+/// <summary>Entry Point.</summary>
+/// <param name="argv">Arguments.</param>
+/// <returns>Exit code (0).</returns>
+let mutexMain argv =
+    let apikey = Environment.GetEnvironmentVariable(CW_API_KEY, EnvironmentVariableTarget.User)
+    if apikey = null then
+        MessageBox.Show("set CW_API_KEY=<your ChatWork API key>", APP_NAME) |> ignore
+        2
+    else
+        Notify.start APP_NAME apikey
+        0
+
+/// <summary>Entry Point.</summary>
+/// <param name="argv">Arguments.</param>
+/// <returns>Exit code.</returns>
+[<EntryPoint>]
+let main argv =
+    use mutex = new Mutex(false, APP_NAME)
+    if mutex.WaitOne(0, false) then mutexMain argv else 1
