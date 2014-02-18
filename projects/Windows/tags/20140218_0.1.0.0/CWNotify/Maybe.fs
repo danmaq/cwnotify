206f1a868c65e0c4457d4bcfd5347dd0826ab7f3
danmaq <danmaq@a959c83c-b6d5-48ed-9b59-49efaf540b30>
2014-02-18 05:50:04 +0000

    ■v0.1.0.0 起動前にユーザー環境変数CW_API_KEYを作成し、そこにChatWork APIキーを入れてください。 起動するとタスクトレイに常駐します。 30秒ごとに未読確認をします。

---
+++ projects/Windows/tags/20140218_0.1.0.0/CWNotify/Maybe.fs
@@ -0,0 +1,17 @@
+﻿/// <summary>Maybe Monad.</summary>
+module Maybe
+
+type MaybeBuilder() =
+
+    /// <summary>Called for <c>let!</c> and <c>do!</c> in computation expressions.</summary>
+    member this.Bind(x, f) =
+        match x with
+            | Some y -> f y
+            | None -> None
+
+    /// <summary>Called for <c>return!</c> in computation expressions.</summary>
+    member this.Return(x) =
+        x |> Some
+
+/// <summary>Maybe Monad.</summary>
+let maybe = MaybeBuilder()
---
+++ projects/Windows/tags/20140218_0.1.0.0/CWNotify/Notify.fs
@@ -0,0 +1,100 @@
+﻿module Notify
+
+open Printf
+open System
+open System.Drawing
+open System.Net
+open System.Reflection
+open System.Runtime.InteropServices
+open System.Windows.Forms
+
+/// <summary>
+/// Retrieves a handle to the foreground window (the window with which the user is currently working).
+/// </summary>
+/// <returns>The return value is a handle to the foreground window.</returns>
+[<DllImport(@"user32")>]
+extern IntPtr GetForegroundWindow();
+
+/// <summary>Returns the Windows desktop window.</summary>
+/// <returns>Identifies the Windows desktop window.</returns>
+[<DllImport(@"user32")>]
+extern IntPtr GetDesktopWindow();
+
+/// <summary>
+/// Brings the thread that created the specified window into the foreground and activates the window.
+/// </summary>
+/// <param name="hWnd">
+/// A handle to the window that should be activated and brought to the foreground. 
+/// </param>
+/// <returns>If the window was brought to the foreground, the return value is nonzero.</returns>
+[<DllImport(@"user32")>]
+extern [<MarshalAs(UnmanagedType.Bool)>] bool SetForegroundWindow(nativeint hWnd);
+
+/// <summary>
+/// Notification icon will not disappear even if pressing the Alt + F4 key.
+/// </summary>
+/// <param name="notify">Instance of notification icon.</param>
+/// <param name="cms">Instance of context menu.</param>
+let cancelAltF4 (notify: NotifyIcon) (cms:ContextMenuStrip) =
+    let setForeground = fun () -> SetForegroundWindow(GetDesktopWindow())
+    cms.PreviewKeyDown.Add <|
+        fun args -> if args.Alt then setForeground() |> ignore
+    cms.Closing.Add <|
+        fun args ->
+            let fi =
+                let bf = BindingFlags.NonPublic ||| BindingFlags.Instance
+                typeof<NotifyIcon>.GetField("window", bf)
+            let handle = (fi.GetValue(notify) :?> NativeWindow).Handle
+            if GetForegroundWindow() = handle then setForeground() |> ignore
+
+/// <summary>
+/// display a notification icon. If icon fails to display, and then retry automatically.
+/// </summary>
+/// <param name="notify">Instance of notification icon.</param>
+/// <param name="prevTick">previous tick time.</param>
+let rec viewIcon (notify: NotifyIcon) prevTick =
+    notify.Visible <- true
+    let tick = Environment.TickCount - prevTick
+    if tick >= 4000 then
+        notify.Visible <- false
+        viewIcon notify tick
+
+let createTimer (notify: NotifyIcon) apikey =
+    let timer = new Timer()
+    timer.Interval <- 30 * 1000
+    timer.Tick.Add <| fun argv ->
+        let data =
+            async {
+                let client = new WebClient()
+                client.Headers.Add("X-ChatWorkToken", apikey)
+                let! result = client.AsyncDownloadString <| new Uri "https://api.chatwork.com/v1/my/status"
+                return result
+            } |> Async.RunSynchronously
+        let num =
+            Maybe.maybe
+                {
+                    let! status = data |> MyStatus.ParseJSON
+                    return status.unread_num
+                }
+        match num with
+            | Some n when n > 0 ->
+                notify.BalloonTipText <- sprintf "%d 件の新着" n
+                notify.ShowBalloonTip 10000;
+                ()
+            | _ -> ()
+        ()
+    timer.Start()
+    timer
+
+let start appName apikey =
+    use notify = new NotifyIcon()
+    notify.Text <- appName
+    notify.ContextMenuStrip <-
+        let cms = new ContextMenuStrip()
+        cancelAltF4 notify cms
+        ("E&xit" |> cms.Items.Add).Click.Add <| fun args -> Application.Exit()
+        cms
+    notify.Icon <- SystemIcons.Application
+    viewIcon notify Environment.TickCount
+    use timer = createTimer notify apikey
+    Application.Run()
