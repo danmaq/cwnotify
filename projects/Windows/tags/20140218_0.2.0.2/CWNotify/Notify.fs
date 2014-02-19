e29c03c60bf1dc864ad393d9c6cc08f23c77acce
danmaq <info@danmaq.com>
2014-02-19 09:26:29 +0000

    ■v0.2.0.2 アイコン追加。 バージョン情報表示機能追加。 音声通知機能追加。 内部コードの大幅整理。

---
+++ projects/Windows/tags/20140218_0.2.0.2/CWNotify/Notify.fs
@@ -0,0 +1,57 @@
+﻿module Notify
+
+open Printf
+open System
+open System.Media
+open System.Windows.Forms
+
+/// <summary>
+/// display a notification icon. If icon fails to display, and then retry automatically.
+/// </summary>
+/// <param name="visible"></param>
+/// <param name="prevTick">previous tick time.</param>
+let rec viewIcon visible prevTick =
+    true |> visible
+    let tick = Environment.TickCount - prevTick
+    if tick >= 4000 then
+        false |> visible
+        viewIcon visible tick
+
+/// <summary></summary>
+/// <param name="notify"></param>
+/// <param name="num"></param>
+let showBalloon (notify:NotifyIcon) num =
+    if num > 0 then
+        SystemSounds.Asterisk.Play()
+        notify.BalloonTipText <- sprintf @"%d 件の新着" num
+        notify.ShowBalloonTip 10000;
+
+/// <summary></summary>
+let showAbout() =
+    let product = Resources.productName
+    let msg =
+        sprintf
+            "%s Version %s\n\n%s\n%s"
+            product Resources.version Resources.copyright Resources.company
+    MessageBox.Show(msg, product) |> ignore
+
+/// <summary></summary>
+/// <param name="notify"></param>
+/// <returns>ContextMenuStrip object.</returns>
+let createContextMenuStrip notify =
+    let cms = new ContextMenuStrip()
+    cms.PreviewKeyDown.Add <| CancelVanish.onPreviewKeyDown
+    cms.Closing.Add <| CancelVanish.onClosing notify
+    ("&About" |> cms.Items.Add).Click.Add <| fun args -> showAbout()
+    ("E&xit" |> cms.Items.Add).Click.Add <| fun args -> Application.Exit()
+    cms
+
+/// <summary></summary>
+let start() =
+    use notify = new NotifyIcon()
+    notify.Text <- Resources.productName
+    notify.ContextMenuStrip <- createContextMenuStrip notify
+    notify.Icon <- Resources.icon
+    viewIcon (fun b -> notify.Visible <- b) Environment.TickCount
+    use timer = TimerLoop.createTimer (notify |> showBalloon)
+    Application.Run()
